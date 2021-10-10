6cc87663cbbfc599c48079eaef6471201bc20db5
aymensp <aymen.smati@esprit.tn>
2021-10-10 23:56:48 +0100

    auth added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+import { withAuthenticator } from 'aws-amplify-react-native'
 
-export default function App() {
+Amplify.configure(config)
+
+ function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -21,3 +25,5 @@ export default function App() {
     );
   }
 }
+export default withAuthenticator(App)
+
