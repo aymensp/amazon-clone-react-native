f261e3c3168223e35ec409b195ce767c1f80b51c
aymensp <aymen.smati@esprit.tn>
2021-10-11 21:26:22 +0100

    Adding grapheql schema

--- App.tsx
+++ App.tsx
@@ -2,24 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import Amplify from 'aws-amplify'
+import Amplify, { Auth } from 'aws-amplify'
 import config from './src/aws-exports'
 import { withAuthenticator } from 'aws-amplify-react-native'
 
 Amplify.configure(config)
 
- function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation  />
+        <Navigation />
         <StatusBar />
       </SafeAreaProvider>
     );
